0e9aa2aa83d8f7dcc2bad9f88bfdcd29515da7f2
Alessandro Carminati <alessandro.carminati@gmail.com>
2024-08-19 10:41:19 +0200

    misc fixes2
    Signed-off-by: Alessandro Carminati <alessandro.carminati@gmail.com>

--- ftrace_it.c
+++ ftrace_it.c
@@ -94,7 +94,7 @@ void set_irq_affinity(int cpu) {
 	snprintf(cpu_mask, sizeof(cpu_mask), "%x", 1 << cpu);
 
 	while (fgets(line, sizeof(line), file)) {
-		if (sscanf(line, " %15[^:]: %*s %*s %*s %127s", irq, irq_name) == 2) {
+		if (sscanf(line, " %15[^:]: %*s %*s %*s %[^\n]", irq, irq_name) == 2) {
 			char *endptr;
 			long irq_num = strtol(irq, &endptr, 10);
 			if (*endptr != '\0') {
