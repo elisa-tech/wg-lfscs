f1c36e4e93e5735aa2cbf3c6fd24ab2335262cf5
Bill Maclean <bill.maclean@codethink.co.uk>
2026-07-07 13:46:46 +0100

    min_prog_trace: readability fix in nop macro
    Signed-off-by: Bill Maclean <bill.maclean@codethink.co.uk>

--- min_prog_trace/min.large.c
+++ min_prog_trace/min.large.c
@@ -1,6 +1,5 @@
 #define NOP1   asm("nop");
-#define NOP2      NOP1     NOP1     NOP1     NOP1
-#define NOP4      NOP2     NOP2     NOP2     NOP2
+#define NOP4      NOP1     NOP1     NOP1     NOP1
 #define NOP16     NOP4     NOP4     NOP4     NOP4
 #define NOP64     NOP16    NOP16    NOP16    NOP16
 #define NOP256    NOP64    NOP64    NOP64    NOP64
